c6b9b02ee2360a57eae302a540fc4f123586c7ce
Caleb Spiess <spiesscaleb@gmail.com>
2021-05-10 06:09:50 -0600

    Fixed a glaring issue with the reward function. Moved code around into logical units. Fixed a bunch of convention stuff; it's all underscores now, at least the parts I wrote. Reward is now output as a total throughout the episode. Added a simple little debug mode which currently just causes stuff to get printed.

--- asciiforce.cpp
+++ asciiforce.cpp
@@ -2,36 +2,50 @@
 // Prints an ascii display of the given width and height around the camera to the console.
 // Charming.
 
-const int WIDTH = 30;
-const int HEIGHT = 20;
+const int WIDTH = 32;
+const int HEIGHT = 24;
 const bool SHOWPLAYER = false;
 const int TIMEOUT_FRAMES = 300;
+const bool DEBUG = false;
 
 class script {
   camera@ cam;
   scene@ g;
   controllable@ player;
-  array<int> rewards;
   array<int> dust;
   array<int> enemy_hp;
   array<int> filth_blocks;  
-  array<int> breaks;
   int place;
+  int last_place;
   uint64 frame;
-  uint32 sinceLastReward;
+  uint32 since_last_reward;
+  int total_reward;
 
-  script() {
-    @cam = get_active_camera();
-    @g = get_scene();
+
+  void init_arrays() {
     for(int i = 0; i < 4; i++) {
-      rewards.insertLast(0);
       dust.insertLast(0);
       enemy_hp.insertLast(0);
       filth_blocks.insertLast(0);
-      breaks.insertLast(0);
     }
+  }
+
+  void reinit_arrays(int dust_c, int hp_c, int filthblock_c) {
+    for(int i = 0; i < 4; i++) {
+      dust[i] = dust_c;
+      enemy_hp[i] = hp_c;
+      filth_blocks[i] = filthblock_c;
+    }
+  }
+  
+  script() {
+    @cam = get_active_camera();
+    @g = get_scene();
+    init_arrays();
     place = 0;
-    sinceLastReward = 0;
+    since_last_reward = 0;
+    total_reward = 0;
+    last_place = 0;
   }
 
   uint8 side_type(uint8 side) {
@@ -42,36 +56,41 @@ class script {
 
   void on_level_start() {
     frame = 0;
-  }
-
-  void on_level_end() {
-    rewards[place] += 100;
-  }
-
-  void step(int entities) {
-    frame++;
-    int lastPlace = place;
-    place = (place + 1) % 4;
-    rewards[place] = 0;
-    int filthblocks = 0;
     int cur_dust = 0;
     int cur_filth = 0;
     int cur_hp = 0;
     g.get_filth_remaining(cur_dust,
                           cur_filth,
                           cur_hp);
-    dust[place] = cur_dust;
-    filth_blocks[place] = cur_filth;
-    enemy_hp[place] = cur_hp;
-    breaks[place] = g.combo_break_count();
-    rewards[place] = (((dust[lastPlace] - dust[place]) + (filth_blocks[lastPlace] - filth_blocks[place])) * 2 +
-                      (enemy_hp[lastPlace] - enemy_hp[place]) * 4 -
-                      (breaks[place] - breaks[lastPlace]) * 30);
-    if(rewards[place] == 0) sinceLastReward++;
-    else sinceLastReward = 0;
+    reinit_arrays(cur_dust, cur_hp, cur_filth);
+  }
+
+  void on_level_end() {
+    total_reward += 100;
+  }
+
+  void update_reward() {
     int cur_reward = 0;
-    for(int i = 0; i < 4; i++) cur_reward += rewards[i];
-                      
+    int cur_dust = 0;
+    int cur_filthblocks = 0;
+    int cur_hp = 0;
+    g.get_filth_remaining(cur_dust,
+                          cur_filthblocks,
+                          cur_hp);
+    dust[place] = cur_dust;
+    filth_blocks[place] = cur_filthblocks;
+    enemy_hp[place] = cur_hp;
+    cur_reward += (((dust[last_place] - dust[place]) + (filth_blocks[last_place] - filth_blocks[place])) * 10 +
+                      (enemy_hp[last_place] - enemy_hp[place]) * 40);
+    if(cur_reward == 0) {
+      since_last_reward++;
+    } else {
+      since_last_reward = 0;
+    }
+    total_reward += cur_reward;
+  }
+
+  array<string> get_screen() {
     array<string> tiles;
     tileinfo@ tile;
     tilefilth@ filth;
@@ -122,11 +141,28 @@ class script {
       int x = int(((player.x() - cam.x()) / 48) + (WIDTH / 2));
       tiles[y-1][x] = 'P'[0];
     }
+    return tiles;
+  }
 
-    for(uint i = 0; i < tiles.length(); i++) puts("}" + tiles[i]);
-    puts(")" + formatInt(cur_reward));
+  void step(int entities) {
+    last_place = place;
+    place = (place + 1) % 4;
+    frame++;
+    update_reward();
+    array<string> screen = get_screen();
+
+    // printing section
+    for(uint i = 0; i < screen.length(); i++) puts("}" + screen[i]);
+    puts(")" + formatInt(total_reward));
     puts("]" + formatInt(frame));
-    if(sinceLastReward > TIMEOUT_FRAMES) puts("`");
-    puts(">~>");    
+    if(since_last_reward > TIMEOUT_FRAMES) puts("`");
+    puts(">~>");
+    if(DEBUG) {
+      // outputs not prefaced are retransmitted after interception,
+      // so these will print
+      for(uint i = 0; i < screen.length(); i++) puts(screen[i]);
+      puts(formatInt(total_reward));
+      puts(formatInt(frame));
+    }
   }
 }
